b95fe9a402fb38e310ee1abb3a27207681734140
es <esrazengn00@gmail.com>
2020-06-09 02:05:02 +0200

    added check for Fibonacci sequence on the row of the clicked cell

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 'use strict';
-
-// OK   Create a 50x50 grid.
-// OK   If you click a cell, all cells in the same row and column get +1.
-// OK   If a cell was empty, it gets the value 1.
-// OK   After every change, the cell lightens up (shortly) in yellow.
-
-// If 5 consecutive numbers of the Fibonacci sequence are next to each other,
-//  the cells lighten up (shortly) in green and
-//  cells will be emptied afterwards.
-// OK   JAVASCRIPT You can use a programming language that you find suited best.
-
 {
   const ROW_NUMBER = 50;
   const COLUMN_NUMBER = 50;
 
-  const getCellValue = (cellId) => {
+  const getCellNumber = (cellId) => {
     const cell = document.getElementById(cellId);
     return Number(cell.textContent);
   };
 
-  const setCellValue = (rowIndex, columnIndex) => {
+  const setCellNumber = (rowIndex, columnIndex) => {
     const cell = document.getElementById(`${rowIndex}_${columnIndex}`);
     const newVal = Number(cell.textContent) + 1;
     cell.textContent = newVal;
@@ -31,10 +20,10 @@
 
   const incrementCells = (rowIndex, columnIndex) => {
     for (let i = 1; i <= 50; i++) {
-      setCellValue(rowIndex, i); // increment the value of the cells that are in the same row as the clicked cell
+      setCellNumber(rowIndex, i); // increment the value of the cells that are in the same row as the clicked cell
 
       if (i !== rowIndex) {
-        setCellValue(i, columnIndex); // increment the value of the cells that are in the same column as the clicked cell
+        setCellNumber(i, columnIndex); // increment the value of the cells that are in the same column as the clicked cell
       }
     }
   };
@@ -42,17 +31,17 @@
   const isPerfectSquare = (number) => Math.sqrt(number) % 1 === 0;
 
   const isFibonacci = (cellId) => {
-    const n = getCellValue(cellId);
+    const number = getCellNumber(cellId);
 
-    const a = 5 * Math.pow(n, 2) + 4;
-    const b = 5 * Math.pow(n, 2) - 4;
+    const caseOne = 5 * Math.pow(number, 2) + 4;
+    const caseTwo = 5 * Math.pow(number, 2) - 4;
 
-    return n !== 0 && (isPerfectSquare(a) || isPerfectSquare(b));
+    return number !== 0 && (isPerfectSquare(caseOne) || isPerfectSquare(caseTwo));
   };
 
   const isAdjacentFibonacci = (cellId, adjacentCellId) => {
-    const nextNumber = getCellValue(adjacentCellId);
-    const number = getCellValue(cellId);
+    const nextNumber = getCellNumber(adjacentCellId);
+    const number = getCellNumber(cellId);
 
     if (nextNumber === 1 && number === 1) {
       return true;
@@ -71,53 +60,130 @@
       void cell.offsetWidth;
       cell.classList.add('backgroundAnimatedGreen');
 
-      setTimeout(() => (cell.textContent = ''), 4000);
+      setTimeout(() => (cell.textContent = ''), 3000);
     });
   };
 
-  const checkFibonacciSequence = (rowIndex, columnIndex) => {
-    for (let k = 1; k <= ROW_NUMBER; k++) {
-      // check columns for each row
-      if (k !== rowIndex) {
-        if (isFibonacci(`${k}_${columnIndex}`)) {
-          let fibArr = [];
-          fibArr.push(`${k}_${columnIndex}`);
+  const insertCellIdToFibonacciArray = (rowIndex, columnIndex, fibArr) => {
+    if (getCellNumber(`${rowIndex}_${columnIndex}`) === 1) {
+      if (fibArr.filter((cellId) => getCellNumber(cellId) === 1).length < 2) {
+        fibArr.push(`${rowIndex}_${columnIndex}`);
+      }
+    } else {
+      fibArr.push(`${rowIndex}_${columnIndex}`);
+    }
+    return fibArr;
+  };
 
-          for (let i = columnIndex - 1; i >= columnIndex - 4; i--) {
-            if (i > 0) {
-              if (fibArr.length < 5) {
-                if (isFibonacci(`${k}_${i}`) && isAdjacentFibonacci(`${k}_${i}`, `${k}_${i + 1}`)) {
-                  if (fibArr.filter((cellId) => getCellValue(cellId) === 1).length < 2) {
-                    fibArr.push(`${k}_${i}`);
-                  }
-                } else {
-                  break;
-                }
-              } else {
-                clearCells(fibArr);
-                fibArr = [];
-              }
-            }
+  // This function checks four columns before the clicked cell's column  to find and delete
+  // five consecutive fibonacci numbers
+  const checkPreviousColumnsForFibonacci = (rowIndex, clickedColumnIndex, fibArr) => {
+    for (
+      let columnIndex = clickedColumnIndex - 1;
+      columnIndex >= clickedColumnIndex - 4;
+      columnIndex--
+    ) {
+      if (columnIndex > 0) {
+        if (fibArr.length < 5) {
+          if (
+            isFibonacci(`${rowIndex}_${columnIndex}`) &&
+            isAdjacentFibonacci(`${rowIndex}_${columnIndex}`, `${rowIndex}_${columnIndex + 1}`)
+          ) {
+            fibArr = insertCellIdToFibonacciArray(rowIndex, columnIndex, fibArr);
+          } else {
+            break;
           }
+        } else {
+          clearCells(fibArr);
+          fibArr = [];
+        }
+      }
+    }
+    return fibArr;
+  };
 
-          if (fibArr.length > 0) {
-            for (let j = columnIndex + 1; j <= columnIndex + 4; j++) {
-              if (fibArr.length < 5) {
-                if (isFibonacci(`${k}_${j}`) && isAdjacentFibonacci(`${k}_${j - 1}`, `${k}_${j}`)) {
-                  fibArr.push(`${k}_${j}`);
-                } else {
-                  break;
-                }
-              } else {
-                clearCells(fibArr);
-                fibArr = [];
-              }
-            }
-          } 
+  // This function checks four columns after the clicked cell's column  to find and delete
+  // five consecutive fibonacci numbers
+  const checkNextColumnsForFibonacci = (rowIndex, clickedColumnIndex, fibArr) => {
+    for (
+      let columnIndex = clickedColumnIndex + 1;
+      columnIndex <= clickedColumnIndex + 4;
+      columnIndex++
+    ) {
+      if (fibArr.length < 5) {
+        if (
+          isFibonacci(`${rowIndex}_${columnIndex}`) &&
+          isAdjacentFibonacci(`${rowIndex}_${columnIndex - 1}`, `${rowIndex}_${columnIndex}`)
+        ) {
+          fibArr = insertCellIdToFibonacciArray(rowIndex, columnIndex, fibArr);
+        } else {
+          break;
         }
       } else {
-        // check row of the clicked cell
-        //console.log(rowIndex, columnIndex);
+        clearCells(fibArr);
+        fibArr = [];
+      }
+    }
+    return fibArr;
+  };
+
+  // This function checks the row of the clicked cell to find and delete
+  // five consecutive fibonacci numbers
+  const checkSameRowForFibonacci = (rowIndex) => {
+    const cells = [];
+    for (let columnIndex = 1; columnIndex <= COLUMN_NUMBER; columnIndex++) {
+      cells.push(`${rowIndex}_${columnIndex}`);
+    }
+
+    cells.reduce((fibArr, cell) => {
+      if (fibArr.length === 0) {
+        if (isFibonacci(cell)) fibArr.push(cell);
+      } else if (isFibonacci(cell) && isAdjacentFibonacci(fibArr[fibArr.length - 1], cell)) {
+        if (getCellNumber(cell) === 1) {
+          if (fibArr.filter((cellId) => getCellNumber(cellId) === 1).length < 2) {
+            fibArr.push(cell);
+          } else {
+            fibArr.shift();
+            fibArr.push(cell);
+          }
+        } else {
+          fibArr.push(cell);
+        }
+      } else {
+        fibArr = [];
+      }
+
+      if (fibArr.length === 5) {
+        clearCells(fibArr);
+        fibArr = [];
+      }
+
+      return fibArr;
+    }, []);
+  };
+
+  const findAndDeleteConsecutiveFibonacci = (clickedRowIndex, clickedColumnIndex) => {
+    for (let rowIndex = 1; rowIndex <= ROW_NUMBER; rowIndex++) {
+      // check four columns before and four columns after the clicked cell for each row for fibonacci sequence EXCEPT the clicked row.
+      if (rowIndex !== clickedRowIndex) {
+        if (isFibonacci(`${rowIndex}_${clickedColumnIndex}`)) {
+          let fibArr = [];
+          fibArr.push(`${rowIndex}_${clickedColumnIndex}`);
+
+          fibArr = checkPreviousColumnsForFibonacci(rowIndex, clickedColumnIndex, fibArr);
+
+          if (fibArr.length > 0) {
+            fibArr = checkNextColumnsForFibonacci(rowIndex, clickedColumnIndex, fibArr);
+          }
+
+          if (fibArr.length === 5) {
+            clearCells(fibArr);
+            fibArr = [];
+          }
+        }
+      } else {
+        // check the same row of the clicked cell for fibonacci sequence.
+        checkSameRowForFibonacci(clickedRowIndex);
       }
     }
   };
@@ -133,10 +199,9 @@
 
         cell.addEventListener('click', () => {
           incrementCells(rowIndex, columnIndex);
-          checkFibonacciSequence(rowIndex, columnIndex);
+          findAndDeleteConsecutiveFibonacci(rowIndex, columnIndex);
         });
       }
-
       table.appendChild(row);
     }
   };
